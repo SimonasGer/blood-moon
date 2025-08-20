0d06664ceccf271528c2bfe0011c9f03f11353df
Simonas Gerulis <simonasgerulis1@gmail.com>
2025-08-20 09:59:28 +0300

    main menu button

---
+++ scripts/MoveScenes.cs
@@ -0,0 +1,18 @@
+using Godot;
+using System;
+
+public partial class MoveScenes : Button
+{
+	[Export] private string sceneName;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		Pressed += OnPressed;
+	}
+
+	private void OnPressed()
+	{
+		GetTree().ChangeSceneToFile($"res://scenes/{sceneName}.tscn");
+	}
+
+}
